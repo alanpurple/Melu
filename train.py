7411a08d7be9b5620d772828e1431873508981f4
Alan Anderson <alanpurple@gmail.com>
2019-09-25 17:59:45 +0900

    add dict

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 from tensorflow.keras import metrics,Model,layers,Sequential,losses
 from connect_db import Session,engine
 from data_models import Movie,User,Rating
+import json
 
 # get all rating
 # divide movies before 1997 and after 1998 ( approximately 8:2 )
@@ -47,6 +48,25 @@ def main():
     rating_exist_new=session.query(Rating).join(User).filter(User.id.in_(user_existing.index)).join(Movie).filter(Movie.year>1997).all()
     rating_new_exist=session.query(Rating).join(User).filter(User.id.in_(user_new.index)).join(Movie).filter(Movie.year<1998).all()
     rating_new_new=session.query(Rating).join(User).filter(User.id.in_(user_new.index)).join(Movie).filter(Movie.year>1997).all()
+
+    actor_dict,director_dict,rated_dict,genre_dict=get_movie_dict('movie_dict.json')
+    author_dict,publisher_dict=get_book_dict('book_dict.json')
+
+def get_movie_dict(movie_dict_file):
+    with open(movie_dict_file,'r') as f:
+        movie_dict=json.load(f)
+    actor_dict=dict(zip(movie_dict['actors'],range(len(movie_dict['actors']))))
+    director_dict=dict(zip(movie_dict['directors'],range(len(movie_dict['directors']))))
+    rated_dict=dict(zip(movie_dict['rateds'],range(len(movie_dict['rateds']))))
+    genre_dict=dict(zip(movie_dict['genres'],range(len(movie_dict['genres']))))
+    return actor_dict,director_dict,rated_dict,genre_dict
+
+def get_book_dict(book_dict_file):
+    with open(book_dict_file,'r') as f:
+        book_dict=json.load(f)
+    author_dict=dict(zip(book_dict['authors'],range(len(book_dict['authors']))))
+    publisher_dict=dict(zip(book_dict['publishers'],range(len(book_dict['publishers']))))
+    return author_dict,publisher_dict
 
 def extract_movie_data(data):
     pass
